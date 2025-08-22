6800670fa9618b6a65e7467c9fd65ca9286cf66b
egl-edu <egl-edu@proton.me>
2025-08-22 12:28:35 -0400

    Renommer dossier de projet godot

--- projet_godot/personnage.gd
+++ projet_godot/personnage.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 func _ready() -> void:
-	print("jeu commencé"	)
+	print("jeu commencé, exécuté quand l'objet est prêt")
 
 # func _process(delta: float) -> void:
-#	print("nouveau frame")
+#	print("nouveau frame, exécuté a chaque frame")
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		print("bouton pressé")
 		# modulate = Color.RED
 		$AnimatedSprite2D.animation = "duck"
+		
+	if event is InputEventMouseButton and event.is_released():
+		print("bouton laissé")
+		$AnimatedSprite2D.animation = "default"
